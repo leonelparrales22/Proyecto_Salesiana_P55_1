13d756c0c17e4535d8b74cbb57c618398b861ab1
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-05 22:28:28 -0500

    Cambios en Foco.php y Ventilador.php

--- Aplicacion_Web/Header.php
+++ Aplicacion_Web/Header.php
@@ -139,8 +139,8 @@ echo
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Accionadores:</h6>
-            <a class="collapse-item" href="utilities-color.html">Ventilador</a>
-            <a class="collapse-item" href="utilities-border.html">Foco</a>
+            <a class="collapse-item" href="Ventilador.php">Ventilador</a>
+            <a class="collapse-item" href="Foco.php">Foco</a>
           </div>
         </div>
       </li>
--- BDD.sql
+++ BDD.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE PRESION (
                 id_presion INT AUTO_INCREMENT NOT NULL,
                 fecha DATETIME NOT NULL,
@@ -39,3 +37,35 @@ INSERT INTO temperatura (fecha,temperatura) VALUES(CURRENT_TIMESTAMP(),23);
 INSERT INTO temperatura (fecha,temperatura) VALUES(CURRENT_TIMESTAMP(),18);
 INSERT INTO temperatura (fecha,temperatura) VALUES(CURRENT_TIMESTAMP(),17);
 INSERT INTO temperatura (fecha,temperatura) VALUES(CURRENT_TIMESTAMP(),22);
+
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),22);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),23);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),24);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),25);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),26);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),27);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),28);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),27);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),26);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),25);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),24);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),23);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),18);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),17);
+INSERT INTO presion (fecha,presion) VALUES(CURRENT_TIMESTAMP(),22);
+
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),22);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),23);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),24);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),25);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),26);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),27);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),28);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),27);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),26);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),25);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),24);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),23);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),18);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),17);
+INSERT INTO humedad (fecha,humedad) VALUES(CURRENT_TIMESTAMP(),22);
