09488c164365ef0ceab556cd470d34d8c1d28471
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-13 00:05:10 -0500

    Update PruebaModuloWifi.ino

--- Arduino/PruebaModuloWifi/PruebaModuloWifi.ino
+++ Arduino/PruebaModuloWifi/PruebaModuloWifi.ino
@@ -1,9 +1,13 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  // but actually the LED is on; this is because
+  // it is active low on the ESP-01)
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
 }
