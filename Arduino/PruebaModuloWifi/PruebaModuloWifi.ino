d27f1db524a39c128fb6c2206d1889e426ccbfc4
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-13 01:28:53 -0500

    Update PruebaModuloWifi.ino

--- Arduino/PruebaModuloWifi/PruebaModuloWifi.ino
+++ Arduino/PruebaModuloWifi/PruebaModuloWifi.ino
@@ -1,15 +1,28 @@
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server(80);
+
+const char* ssid="Claro_CHIMBA"; 
+const char* password="5108138419655";
+
+String Website;
+
+void WebsiteContent(){
+  Website="<html>Hello</html>";
+  server.send(200,"text/html",Website);
 }
 
-// the loop function runs over and over again forever
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid,password);
+  while(WiFi.status()!=WL_CONNECTED)delay(500);
+  WiFi.mode(WIFI_STA);
+  Serial.print(WiFi.localIP());
+  server.on("/",WebsiteContent);
+  server.begin();
+}
+
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  // but actually the LED is on; this is because
-  // it is active low on the ESP-01)
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
-  Serial.print("LEONEL PARRALES");
+  server.handleClient();
 }
