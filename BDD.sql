59c1c9d4f132fcd3d30cb7976e632d745d583c13
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-13 21:15:46 -0500

    Update BDD.sql

--- BDD.sql
+++ BDD.sql
@@ -1,3 +1,35 @@
+
+
+CREATE TABLE ACTIVADORES (
+                id_activador INT AUTO_INCREMENT NOT NULL,
+                nombre VARCHAR(20) NOT NULL,
+                estado BOOLEAN NOT NULL,
+                PRIMARY KEY (id_activador)
+);
+
+CREATE TABLE PRESION (
+                id_presion INT AUTO_INCREMENT NOT NULL,
+                fecha DATETIME NOT NULL,
+                presion INT NOT NULL,
+                PRIMARY KEY (id_presion)
+);
+
+
+CREATE TABLE HUMEDAD (
+                id_humedad INT AUTO_INCREMENT NOT NULL,
+                fecha DATETIME NOT NULL,
+                humedad INT NOT NULL,
+                PRIMARY KEY (id_humedad)
+);
+
+
+CREATE TABLE TEMPERATURA (
+                id_temperatura INT AUTO_INCREMENT NOT NULL,
+                fecha DATETIME NOT NULL,
+                temperatura INT NOT NULL,
+                PRIMARY KEY (id_temperatura)
+);
+
 CREATE TABLE PRESION (
                 id_presion INT AUTO_INCREMENT NOT NULL,
                 fecha DATETIME NOT NULL,
