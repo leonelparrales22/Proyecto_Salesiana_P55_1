fbf4c910eac6730874ae94e2df81ff108f406153
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-14 19:04:44 -0500

    Update MyUpdate2.php

--- CursoAjax/3Ejemplo/MyUpdate2.php
+++ CursoAjax/3Ejemplo/MyUpdate2.php
@@ -2,9 +2,5 @@
 include('database.php');
 $query = 'UPDATE activadores SET estado=false WHERE nombre="VENTILADOR";';
 mysqli_query($conexion, $query);
-
-$query = 'UPDATE activadores SET estado=false WHERE nombre="VENTILADOR";';
-mysqli_query($conexion, $query);
-
 mysqli_close($conexion);
 ?>
