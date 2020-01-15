08baf4bd48737c4f062e272fd4f50d0d0c9fa8ce
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-14 19:06:35 -0500

    Update BDDVentilador1.php

--- CodeIgniter/BDDVentilador1.php
+++ CodeIgniter/BDDVentilador1.php
@@ -2,5 +2,9 @@
 include('database.php');
 $query = 'UPDATE activadores SET estado=true WHERE nombre="VENTILADOR";';
 mysqli_query($conexion, $query);
+
+$query = 'INSERT INTO registro (fecha,descripcion) VALUES(CURRENT_TIMESTAMP(),"VENTILADOR SE ENCENDIO");';
+mysqli_query($conexion, $query);
+
 mysqli_close($conexion);
 ?>
