94d455ca33ce4ef3de2a290ea3d34e3291e79c38
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-14 19:09:40 -0500

    Update BDDFoco0.php and BDDFoco1.php

--- CodeIgniter/BDDFoco1.php
+++ CodeIgniter/BDDFoco1.php
@@ -2,5 +2,9 @@
 include('database.php');
 $query = 'UPDATE activadores SET estado=true WHERE nombre="FOCO";';
 mysqli_query($conexion, $query);
+
+$query = 'INSERT INTO registro (fecha,descripcion) VALUES(CURRENT_TIMESTAMP(),"FOCO SE ENCENDIO");';
+mysqli_query($conexion, $query);
+
 mysqli_close($conexion);
 ?>
