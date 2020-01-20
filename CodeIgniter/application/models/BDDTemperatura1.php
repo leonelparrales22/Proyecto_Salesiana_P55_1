81b9757a4f970bced6ef9f44542c99a36e558827
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-20 00:39:51 -0500

    Update BDDTemperatura1.php

--- CodeIgniter/application/models/BDDTemperatura1.php
+++ CodeIgniter/application/models/BDDTemperatura1.php
@@ -1,31 +1,30 @@
 <?php
-// $conexion = mysqli_connect("localhost", "root", "", "p1") or
-// die("Problemas con la conexión");
+$conexion = mysqli_connect("localhost", "root", "", "p1") or
+die("Problemas con la conexión");
 
-// $registros = mysqli_query($conexion, "select * from temperatura ORDER BY fecha DESC LIMIT 1") or
-// die("Problemas en el select:" . mysqli_error($conexion));
+$registros = mysqli_query($conexion, "select * from temperatura ORDER BY fecha DESC LIMIT 1") or
+die("Problemas en el select:" . mysqli_error($conexion));
 
-// while ($reg = mysqli_fetch_array($registros)) {
-//    echo $reg['temperatura'];
-// }
-// mysqli_close($conexion);
-
-
+while ($reg = mysqli_fetch_array($registros)) {
+   echo $reg['temperatura'];
+}
+mysqli_close($conexion);
 
 
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class BDDTemperatura1 extends CI_Model {
+// defined('BASEPATH') OR exit('No direct script access allowed');
 
-    function __construct(){
-        parent::__construct();
-        $this->load->database();
-        $query = $this->db->query('select * from temperatura ORDER BY fecha DESC LIMIT 1');
-        foreach ($query->result_array() as $reg) {
-            echo $reg['temperatura'];
-        }
-    }
+// class BDDTemperatura1 extends CI_Model {
+
+//     function __construct(){
+//         parent::__construct();
+//         $this->load->database();
+//         $query = $this->db->query('select * from temperatura ORDER BY fecha DESC LIMIT 1');
+//         foreach ($query->result_array() as $reg) {
+//             echo $reg['temperatura'];
+//         }
+//     }
     
-    }
+//     }
     
 ?>
