94f81c3873140361f0f2e57e618a62cb7e2e0e5a
1eduar <52583371+1eduar@users.noreply.github.com>
2020-01-22 14:46:04 -0800

    Create enviardatos.php

---
+++ Arduino/enviardatos.php
@@ -0,0 +1,18 @@
+<?php
+$conexion = mysqli_connect("localhost","phpmyadmin","wq","phpmyadmin") or
+die("Problemas con la conexion");
+$chipid = $_POST ['chipid'];
+$temperatura = $_POST ['temperatura'];
+$humedad = $_POST ['humedad'];
+
+mysqli_query($conexion, "INSERT INTO TEMPERATURA(`id_temperatura`, `chipid`, `fecha`, `temperatura`) VALUES (null,'$chipid',CURRENT_TIMESTAMP,'$temperatura')")
+or die(mysqli_error($conexion));
+echo "se inserto tem.";
+
+mysqli_query($conexion, "INSERT INTO HUMEDAD(`id_humedad`, `chipid`, `fecha`, `humedad`) VALUES (null,'$chipid',CURRENT_TIMESTAMP,'$humedad')")
+or die(mysqli_error($conexion));
+echo "se inserto hume.";
+
+?>
+
+
