e36d4fae508a60924356475ea70205f14f497c43
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-22 21:54:41 -0500

    Update TablaPresion.php

--- Codeigniter/application/views/presion/TablaPresion.php
+++ Codeigniter/application/views/presion/TablaPresion.php
@@ -94,21 +94,21 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="<?php echo base_url();?>vendor/jquery/jquery.min.js"></script>
-  <script src="<?php echo base_url();?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url();?>application/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>application/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="<?php echo base_url();?>vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?php echo base_url();?>application/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="<?php echo base_url();?>js/sb-admin-2.min.js"></script>
+  <script src="<?php echo base_url();?>application/assets/js/sb-admin-2.min.js"></script>
 
   <!-- Page level plugins -->
-  <script src="<?php echo base_url();?>vendor/datatables/jquery.dataTables.min.js"></script>
-  <script src="<?php echo base_url();?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
+  <script src="<?php echo base_url();?>application/assets/vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="<?php echo base_url();?>application/assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
   <!-- Page level custom scripts -->
-  <script src="<?php echo base_url();?>js/demo/datatables-demo.js"></script>
+  <script src="<?php echo base_url();?>application/assets/js/demo/datatables-demo.js"></script>
 
 </body>
 
