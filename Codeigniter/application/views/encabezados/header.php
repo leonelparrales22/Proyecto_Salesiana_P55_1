8692bc1f37d2e62a9100881d1149c1123b0e8d5d
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-22 21:46:07 -0500

    Update header.php

--- Codeigniter/application/views/encabezados/header.php
+++ Codeigniter/application/views/encabezados/header.php
@@ -12,16 +12,16 @@
   <title>Dashboard</title>
 
   <!-- Custom fonts for this template-->
-  <link href="<?php echo base_url();?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url();?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="<?php echo base_url();?>css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/css/sb-admin-2.min.css" rel="stylesheet">
 
   <!-- Custom styles for this page -->
-  <link href="<?php echo base_url();?>vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 
 
@@ -399,7 +399,7 @@
 
               <span class="mr-2 d-none d-lg-inline text-gray-600 small">Juma W., Fernandez J., Morales D., Parrales L, Quisupangui E.,
                 Velasco B.</span>
-              <img class="img-profile rounded-circle" src="<?php echo base_url();?>img/Logo-Salesiana.png">
+              <img class="img-profile rounded-circle" src="<?php echo base_url();?>assets/img/Logo-Salesiana.png">
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
