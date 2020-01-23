4184515fcf28571d268ec72c3f0595146f178766
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-01-22 21:21:49 -0500

    Update app2.js

--- Codeigniter/application/assets/js/app2.js
+++ Codeigniter/application/assets/js/app2.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    window.alert("hola");
     $("input.active").click(function() {
         var check_active = $(this).is(':checked') ? 1 : 0;
         if(parseInt(check_active)==1){
